5282a37a04311db239beec7bfffb8fb6c6046c9e
joeflack4 <joeflack4@gmail.com>
2023-04-12 09:53:54 -0400

    - Add: Params: Implemented split_by_vocab and skip_if_in_release. - Update: .gitignore: JavaScript stuff for possible necessity for YARRRML packages. - Update: Prefix N3C --> OMOP

--- n3c_owl_ingest/n3c_owl_ingest.py
+++ n3c_owl_ingest/n3c_owl_ingest.py
@@ -15,14 +15,18 @@ TODO's
 """
 import os
 import subprocess
+# import sys
+from datetime import datetime
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import pandas as pd
 
 CURIE = str
 SRC_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = SRC_DIR.parent
+# ROBOT_PATH = 'robot'  # 2023/04/19: Strangely, this worked. Then, an hour later, only /usr/local/bin/robot worked
+ROBOT_PATH = '/usr/local/bin/robot'
 RELEASE_DIR = PROJECT_DIR / 'release'
 TERMHUB_CSETS_DIR = PROJECT_DIR / 'termhub-csets'
 DATASETS_DIR = TERMHUB_CSETS_DIR / 'datasets' / 'prepped_files'
@@ -35,7 +39,8 @@ CONCEPT_RELATIONSHIP_SUBSUMES_CSV = DATASETS_DIR / 'concept_relationship_subsume
 OUTPATH_TEMPLATE = RELEASE_DIR / 'n3c.robot.template.tsv'
 OUTPATH_OWL = RELEASE_DIR / 'n3c.owl'
 ONTOLOGY_IRI = 'http://purl.obolibrary.org/obo/N3C/ontology'
-PREFIX_MAP_STR = 'N3C: http://purl.obolibrary.org/obo/N3C_'
+# PREFIX_MAP_STR = 'N3C: http://purl.obolibrary.org/obo/N3C_'
+PREFIX_MAP_STR = 'OMOP: https://athena.ohdsi.org/search-terms/terms/'
 CONCEPT_DTYPES = {
     'concept_id': str,  # is int, but we're just serializing, not manipulating
     'concept_name': str,
@@ -70,25 +75,18 @@ ROBOT_SUBHEADER = {
     'invalid_reason': 'A N3C:invalid_reason',
 }
 
-def run_ingest():
-    """Run the ingest"""
-    # Read inputs
-    # - concept_relationship table
-    concept_subsumes_df = pd.read_csv(CONCEPT_RELATIONSHIP_SUBSUMES_CSV, dtype=CONCEPT_RELATIONSHIP_DTYPES).fillna('')
-    concept_subsumes_df = concept_subsumes_df[concept_subsumes_df.invalid_reason == '']
-    concept_subclass_ofs: Dict[List[int]] = {}
-    for row in concept_subsumes_df.itertuples(index=False):
-        # noinspection PyUnresolvedReferences It_doesnt_know_that_row_is_a_namedtuple
-        concept_subclass_ofs.setdefault(row.concept_id_2, []).append(row.concept_id_1)
-    # how_many_parents_gt_1 = [len(x) > 1 for x in concept_subclass_ofs.values()].count(True)  # analysis: 215,489 / ~7m
-    num_subclass_cols = max([len(x) for x in concept_subclass_ofs.values()])  # determined by greatest num parents
-    # - concept table
-    concept_df = pd.read_csv(CONCEPT_CSV, index_col='concept_id', dtype=CONCEPT_DTYPES).fillna('')
 
-    # Construct robot template
-    # - Convert concept table to robot template format
+def create_robot_template(
+    concept_subclass_ofs: Dict, num_subclass_cols: int, df: pd.DataFrame, vocab_id: str = '',
+    outpath: Union[Path, str] = OUTPATH_OWL
+):
+    """Create robot template"""
+    outpath_template = str(outpath).replace('.owl', '.robot.template.tsv')
+    # concepts_in_domain = set(df.index)
+
     d: Dict[CURIE, Dict[str, str]] = {}
-    for row in concept_df.itertuples():
+    for row in df.itertuples():
+        # todo: faster if I build curies beforehand
         # noinspection PyUnresolvedReferences It_doesnt_know_that_row_is_a_namedtuple
         curie_omop = f'N3C:{row.Index}'
         # noinspection PyUnresolvedReferences It_doesnt_know_that_row_is_a_namedtuple
@@ -110,6 +108,8 @@ def run_ingest():
             try:
                 # noinspection PyUnresolvedReferences It_doesnt_know_that_row_is_a_namedtuple
                 parent_concept_id = concept_subclass_ofs[row.Index][i]
+                # if parent_concept_id not in concepts_in_domain:
+                #     print(f'Warning: Parent not in vocab. This causes issue w/ our strategy to split table.: {vocab_id}', file=sys.stderr)
                 parent_curie_omop = f'N3C:{parent_concept_id}'
                 row_dict[f'Parent Class {i + 1}'] = parent_curie_omop
             except (KeyError, IndexError):
@@ -121,17 +121,64 @@ def run_ingest():
         ROBOT_SUBHEADER[f'Parent Class {i + 1}'] = f'SC %'
     # - Create CSV
     robot_df = pd.DataFrame([ROBOT_SUBHEADER] + list(d.values()))
-    robot_df.to_csv(OUTPATH_TEMPLATE, index=False, sep='\t')
+    robot_df.to_csv(outpath_template, index=False, sep='\t')
 
     # Convert to OWL
-    subprocess.run([
-        'robot', 'template',
-        '--template', OUTPATH_TEMPLATE,
-        '--prefix', PREFIX_MAP_STR,
-        '--ontology-iri', ONTOLOGY_IRI,
-        '--output', OUTPATH_OWL
-    ])
+    command = f'export ROBOT_JAVA_ARGS=-Xmx28G; ' \
+              f'"{ROBOT_PATH}" template ' \
+              f'--template "{outpath_template}" ' \
+              f'--prefix "{PREFIX_MAP_STR}" ' \
+              f'--ontology-iri "{ONTOLOGY_IRI}" ' \
+              f'--output "{outpath}"'
+    results = subprocess.run(command, capture_output=True, shell=True)
+    print(results.stdout.decode())
+    return results
+
+
+def run_ingest(split_by_vocab: bool = False, skip_if_in_release: bool = True):
+    """Run the ingest"""
+    # Read inputs
+    # - concept_relationship table
+    concept_subsumes_df = pd.read_csv(CONCEPT_RELATIONSHIP_SUBSUMES_CSV, dtype=CONCEPT_RELATIONSHIP_DTYPES).fillna('')
+    concept_subsumes_df = concept_subsumes_df[concept_subsumes_df.invalid_reason == '']
+    concept_subclass_ofs: Dict[List[int]] = {}
+    for row in concept_subsumes_df.itertuples(index=False):
+        # noinspection PyUnresolvedReferences It_doesnt_know_that_row_is_a_namedtuple
+        concept_subclass_ofs.setdefault(row.concept_id_2, []).append(row.concept_id_1)
+    # how_many_parents_gt_1 = [len(x) > 1 for x in concept_subclass_ofs.values()].count(True)  # analysis: 215,489 / ~7m
+    num_subclass_cols = max([len(x) for x in concept_subclass_ofs.values()])  # determined by greatest num parents
+    # - concept table
+    concept_df = pd.read_csv(CONCEPT_CSV, index_col='concept_id', dtype=CONCEPT_DTYPES).fillna('')
+
+    # Construct robot template
+    # - Convert concept table to robot template format
+    if split_by_vocab:
+        grouped = concept_df.groupby('vocabulary_id')
+        i = 1
+        name: str
+        for name, group in grouped:
+            name = name if name else 'Metadata'  # AFAIK, there's just 1 concept "No matching concept" for this
+            t_i1 = datetime.now()
+            print('Starting vocab', i, 'of', len(grouped), ':', name)
+            outfile = f'n3c-{name}.owl'
+            outpath = RELEASE_DIR / outfile
+            if skip_if_in_release and os.path.exists(outpath):
+                i += 1
+                continue
+            # noinspection PyBroadException
+            try:
+                create_robot_template(concept_subclass_ofs, num_subclass_cols, group, name, outpath)
+            except Exception:
+                os.remove(outpath)
+            t_i2 = datetime.now()
+            print(f'Vocab {name} finished in {(t_i2 - t_i1).seconds} seconds')
+            i += 1
+    else:
+        create_robot_template(concept_subclass_ofs, num_subclass_cols, concept_df)
 
 
 if __name__ == '__main__':
+    t1 = datetime.now()
     run_ingest()
+    t2 = datetime.now()
+    print(f'Finished in {(t2 - t1).seconds} seconds')
