8086cfc0dce5c1bbda58151e46fa736f49dc8a9a
joeflack4 <joeflack4@gmail.com>
2023-10-08 21:06:09 -0400

    1.3.3 - Bugfix: 'omoprel' namespace missing from OWL.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://github.com/HOT-Ecosystem/omop2owl-vocab/'
 EMAIL = 'jflack@jhu.edu'
 AUTHOR = 'Joe Flack'
 REQUIRES_PYTHON = '>=3.9.0'
-VERSION = '1.3.2'
+VERSION = '1.3.3'
 
 # Requirements
 REQUIRED = [
